3201bcd4478afba3bd895d7a50b60c1fe5fb0309
Mkilord <mkilord6666@gmail.com>
2025-03-07 12:33:35 +0300

    Init project

---
+++ src/main/java/ru/mkilord/wetherwebapp/WetherWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.mkilord.wetherwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetherWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WetherWebAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/mkilord/wetherwebapp/ui/MainView.java
@@ -0,0 +1,13 @@
+package ru.mkilord.wetherwebapp.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+
+@SpringComponent
+@Route("")
+public class MainView extends VerticalLayout {
+    public MainView() {
+
+    }
+}
